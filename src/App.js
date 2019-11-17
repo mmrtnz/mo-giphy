64b9bdc00dc0de9125c6531d5cf1b5cc8d770742
mmrtnz <muhammad_martinez@yahoo.com>
2019-11-17 11:19:03 -0600

    Stubs out header with typography

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 // External Dependencies
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import {
+  createMuiTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from '@material-ui/core/styles';
 
+// Local Variables
+let theme = createMuiTheme();
+theme = responsiveFontSizes(theme);
 
 // Component Definition
 function App() {
   return (
-    <div>
-      Hello world
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <Typography variant="h3">Mo Gifs</Typography>
+      </div>
+    </ThemeProvider>
   );
 }
 
